168011246cc15a7d2e5dee99a3a290aa620c398b
marianneleal <marianneleal93@gmail.com>
2021-12-08 18:17:07 +0100

    provisional restults page

--- lib/results_page.dart
+++ lib/results_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'bmi_theme.dart';
+import 'constants.dart';
+import 'main.dart';
+import 'my_container.dart';
 
 class ResultsPage extends StatelessWidget {
   @override
@@ -11,6 +14,33 @@ class ResultsPage extends StatelessWidget {
         title: Text(
           'your bmi result',
           style: BmiTheme.darkTextTheme.headline1,
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Expanded(
+              child: myContainer(
+                color: Color(0xFF0c3d12),
+                containerChild: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text('Normal', style: kResultTextStyle),
+                    Text(
+                      '$calculatedBMI',
+                      style: kNumberTextStyle,
+                    ),
+                    Text(
+                      'Your weight is within the normal range.',
+                      style: BmiTheme.darkTextTheme.bodyText2,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
