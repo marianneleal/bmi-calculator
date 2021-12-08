40aeacc9caad04a37756d80ee847b131333b944b
marianneleal <marianneleal93@gmail.com>
2021-12-08 18:48:12 +0100

    exported calculator logic to bmi brain

---
+++ lib/bmi_brain.dart
@@ -0,0 +1,24 @@
+import 'dart:math';
+
+class BMIBrain {
+  final int height;
+  final double weight;
+  double _bmi;
+
+  BMIBrain({this.height, this.weight});
+
+  String calculateBMI() {
+    _bmi = weight / pow(height / 100, 2);
+    return _bmi.toStringAsFixed(1);
+  }
+
+  String judgeResult() {
+    if (_bmi >= 25) {
+      return 'Overweight';
+    } else if (_bmi > 18.5) {
+      return 'Normal';
+    } else {
+      return 'Underweight';
+    }
+  }
+}
